9df5a08f52a502b17cc4e7f8f5de55bd90625c36
picasso09 <119201284+picasso09@users.noreply.github.com>
2022-11-27 22:30:03 +0700

    Add files via upload

---
+++ post-fs-data.sh
@@ -0,0 +1,4 @@
+#!/system/bin/sh
+MODDIR=${0%/*}
+mount --bind $MODDIR/oppo_product/media/bootanimation/bootanimation.zip /oppo_product/media/bootanimation/bootanimation.zip
+mount --bind $MODDIR/oppo_product/media/bootanimation/bootanimation.zip /oppo_product/media/bootanimation/bootanimation.zip
